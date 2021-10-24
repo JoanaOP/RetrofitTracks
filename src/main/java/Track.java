4f3596be0e55ff59b4dd021e89247c240a0279c8
JoanaOP <joana.orpella@gmail.com>
2021-10-24 18:59:02 +0200

    Creacio Retrofit

---
+++ src/main/java/Track.java
@@ -0,0 +1,31 @@
+public class Track {
+    String id;
+    String title;
+    String singer;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id=id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+}
---
+++ src/main/java/TracksAPI.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface TracksAPI {
+    @GET("tracks")
+    Call<List<Track>> loadTracks();
+}
